1e28553ce47706f32e144d8346dee39d821e9ee8
gigiguan <gigikwan180@gmail.com>
2021-11-17 16:13:58 -0800

    customize about page

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template, request
+import requests
+import json
 
 # create an instance of flask object
 app = Flask(__name__)
@@ -24,7 +26,17 @@ def christina():
 
 @app.route("/gigi/")
 def gigi():
-    return render_template("team/gigi.html")
+    url = "https://random-words-with-pronunciation.p.rapidapi.com/word"
+
+    headers = {
+        'x-rapidapi-host': "random-words-with-pronunciation.p.rapidapi.com",
+        'x-rapidapi-key': "4ab4681ba9mshf17197c9d59be44p17d1edjsnabe7ccc22eb5"
+    }
+
+    response = requests.request("GET", url, headers=headers)
+    output = json.loads(response.text)
+    print(response.text)
+    return render_template("team/gigi.html", Y=output)
 
 # from image import hide_msg
 # @app.route("/rgbhide")
