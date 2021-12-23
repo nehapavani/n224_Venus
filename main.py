9c23102c5f42aba78efd04067396a9679ba24f7e
Christina Lee <christinlee367@gmail.com>
2021-12-23 00:12:48 -0800

    experiment/pbl :

--- main.py
+++ main.py
@@ -28,6 +28,10 @@ def tracy_jarman():
 def anna_mani():
     return render_template("women_disasters/anna_mani.html")
 
+@app.route("/lydia_sijp/")
+def lydia_sijp():
+    return render_template("women_disasters/lydia_sijp.html")
+
 @app.route("/about/")
 def about():
     return render_template("layouts/about.html")
