b77cc19a93518dcf14b71a4594d3e7ef476b2799
gigi <gigikwan180@gmail.com>
2022-02-28 14:57:33 -0800

    earthquake audio

---
+++ templates/team/tttswap.py
@@ -0,0 +1,13 @@
+age1=int(input())
+age2=int(input())
+def swap(age1, age2):
+    if age1 > age2:
+        temp1 = age1
+        temp2 = age2
+        age1 = temp2
+        age2 = temp1
+        return age1, age2
+
+if __name__ == “__main__“:
+    x,y = swap(age1, age2)
+    print (x,y)
