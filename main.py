09ec4bdad20962583e21c9c9c3b6ce3423a7d4e4
nehapavani <nehapavani@gmail.com>
2022-04-04 13:10:50 -0700

    website running

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask import render_template, request
+import requests
+import json
+app = Flask(__name__)
+@app.route("/")
+# map URL route for function below
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
