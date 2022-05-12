29633d568c8983fe6a774ef43311226a96958f10
PrishaB <prishab.1220@gmail.com>
2022-05-12 13:12:20 -0700

    Prisha - Login stuff plus new image folder

--- main.py
+++ main.py
@@ -2,10 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask import render_template, request
+from cruddy.app_crud import app_crud
+from cruddy.app_crud_api import app_crud_api
 import requests
 import json
 
 app = Flask(__name__)
+app.register_blueprint(app_crud)
+app.register_blueprint(app_crud_api)
 
 
 @app.route("/")
